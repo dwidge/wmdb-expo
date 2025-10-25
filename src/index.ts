fe1352791e992ab8f9919f7349884e31589dadbe
dwidge <dwidge+gh@gmail.com>
2025-10-25 14:58:09 +0200

    refactor(makeWatermelonApiHooks): Rename useWatermelonLocal to makeWatermelonApiHooks

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@
  * @module wmdb-expo
  * @description A module that provides WatermelonDB adapters for Expo applications.
  */
+export * from "./makeWatermelonApiHooks.js";
 export * from "./Sync.js";
 export * from "./syncTables.js";
-export * from "./useWatermelonLocal.js";
 export * from "./useWatermelonSync.js";
 export * from "./useWmdbMetrics.js";
 export * from "./useWmdbQuery.js";
